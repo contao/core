47dd4a8295f9693766ce1231276e8a7563da30cd
Leo Feyer <github@contao.org>
2010-04-08 08:39:20 +0000

    Removed the Mediabox autoload code block (#1778)

--- plugins/mediabox/js/mediabox_src.js
+++ plugins/mediabox/js/mediabox_src.js
@@ -912,17 +912,4 @@ var Mediabox;
 		return false;
 	}
 })();
-
-// AUTOLOAD CODE BLOCK
-Mediabox.scanPage = function() {
-//	$$('#mb_').each(function(hide) { hide.set('display', 'none'); });
-	var links = $$("a").filter(function(el) {
-		return el.rel && el.rel.test(/^lightbox/i);
-	});
-	$$(links).mediabox({/* Put custom options here */}, null, function(el) {
-		var rel0 = this.rel.replace(/[[]|]/gi," ");
-		var relsize = rel0.split(" ");
-		return (this == el) || ((this.rel.length > 8) && el.rel.match(relsize[1]));
-	});
-};
-window.addEvent("domready", Mediabox.scanPage);
+// PATCH: remove autoload block
