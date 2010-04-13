514fd0c813c6bd0060dd7cf80d84b0f4b1f338c4
Leo Feyer <github@contao.org>
2010-04-13 08:53:11 +0000

    Added a patch for the new TinyMCE autoresize feature (#1821)

--- plugins/tinyMCE/plugins/autoresize/editor_plugin_src.js
+++ plugins/tinyMCE/plugins/autoresize/editor_plugin_src.js
@@ -37,6 +37,9 @@
 			function resize() {
 				var d = ed.getDoc(), b = d.body, de = d.documentElement, DOM = tinymce.DOM, resizeHeight = t.autoresize_min_height, myHeight;
 
+				// PATCH: Safari fix
+				DOM.setStyle(DOM.get(ed.id + '_ifr'), 'height', '0px');
+
 				// Get height differently depending on the browser used
 				myHeight = tinymce.isIE ? b.scrollHeight : de.offsetHeight;
 
