7418d34c4376e4f932becd3bd07cc5d56234575c
Leo Feyer <github@contao.org>
2010-06-24 11:28:07 +0000

    Set the permissions of new folders to 755 when using the Safe Mode Hack (#2120)

--- system/libraries/FTP.php
+++ system/libraries/FTP.php
@@ -141,8 +141,10 @@ class FTP extends Files
 	{
 		$this->connect();
 		$this->validate($strDirectory);
+		$return = @ftp_mkdir($this->resConnection, $GLOBALS['TL_CONFIG']['ftpPath'] . $strDirectory) ? true : false;
+		$this->chmod($strDirectory, 0755);
 
-		return @ftp_mkdir($this->resConnection, $GLOBALS['TL_CONFIG']['ftpPath'] . $strDirectory) ? true : false;
+		return $return;
 	}
 
 
