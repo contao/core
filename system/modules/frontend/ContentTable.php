de86efbc6feb036b7b9cdfafd1554cb6a8862e08
Leo Feyer <github@contao.org>
2011-03-02 13:03:27 +0000

    Spelling mistake

--- system/modules/frontend/ContentTable.php
+++ system/modules/frontend/ContentTable.php
@@ -1,4 +1,4 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
 
 /**
  * Contao Open Source CMS
