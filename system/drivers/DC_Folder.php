4a741c3ef830408837486c8cc064e37e49dc7ab5
Leo Feyer <github@contao.org>
2011-03-11 16:05:03 +0000

    The code editor did not show up in the file manager (#2922)

--- system/drivers/DC_Folder.php
+++ system/drivers/DC_Folder.php
@@ -1403,7 +1403,8 @@ window.addEvent(\'domready\', function()
 		// Prepare the code editor
 		if ($GLOBALS['TL_CONFIG']['useCE'])
 		{
-			$this->ceField = 'ctrl_source';
+			$this->ceFields = array('ctrl_source');
+			$this->ceField = 'ctrl_source'; // Backwards compatibility
 			$this->language = $GLOBALS['TL_LANGUAGE'];
 			$this->extension = $objFile->extension;
 
