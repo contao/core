88291d7868f4cd1cc670aa22a1515c713586de5d
Leo Feyer <github@contao.org>
2011-05-25 16:33:44 +0000

    Added an "updatePersonalData" to the "personal data" module (#2313)

--- system/modules/frontend/ModulePersonalData.php
+++ system/modules/frontend/ModulePersonalData.php
@@ -249,6 +249,16 @@ class ModulePersonalData extends Module
 		// Redirect or reload if there was no error
 		if ($this->Input->post('FORM_SUBMIT') == 'tl_member_' . $this->id && !$doNotSubmit)
 		{
+			// HOOK: updated personal data
+			if (isset($GLOBALS['TL_HOOKS']['updatePersonalData']) && is_array($GLOBALS['TL_HOOKS']['updatePersonalData']))
+			{
+				foreach ($GLOBALS['TL_HOOKS']['updatePersonalData'] as $callback)
+				{
+					$this->import($callback[0]);
+					$this->$callback[0]->$callback[1]($this->User, $_SESSION['FORM_DATA']);
+				}
+			}
+			
 			$this->jumpToOrReload($this->jumpTo);
 		}
 
