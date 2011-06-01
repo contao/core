8b8a40d3718b164b730d9678254a2d16e994d697
Leo Feyer <github@contao.org>
2011-06-01 10:25:17 +0000

    Version 2.10.beta1 candidate

--- system/modules/rep_base/RepositorySettings.php
+++ system/modules/rep_base/RepositorySettings.php
@@ -42,6 +42,7 @@ define('REPOSITORY_SOAPCACHE', true);
 
 // valid core versions in descending order
 define('REPOSITORY_COREVERSIONS',
+		'20100003,20100003;'.	// 2.10.0 beta1
 		'20090059,20090059;'.	// 2.9.5 stable
 		'20090049,20090049;'.	// 2.9.4 stable
 		'20090039,20090039;'.	// 2.9.3 stable
