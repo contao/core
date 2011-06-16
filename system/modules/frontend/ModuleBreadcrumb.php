0c16344f400014a35657a25d87be43c5ab14acd2
Leo Feyer <github@contao.org>
2011-06-16 08:52:51 +0000

    Added a "generateBreadcrumb" hook to modify breadcrumb menu items (#1771)

--- system/modules/frontend/ModuleBreadcrumb.php
+++ system/modules/frontend/ModuleBreadcrumb.php
@@ -229,6 +229,16 @@ class ModuleBreadcrumb extends Module
 			);
 		}
 
+		// HOOK: add custom logic
+		if (isset($GLOBALS['TL_HOOKS']['generateBreadcrumb']) && is_array($GLOBALS['TL_HOOKS']['generateBreadcrumb']))
+		{
+			foreach ($GLOBALS['TL_HOOKS']['generateBreadcrumb'] as $callback)
+			{
+				$this->import($callback[0]);
+				$items = $this->$callback[0]->$callback[1]($items);
+			}
+		}
+
 		$this->Template->items = $items;
 	}
 }
