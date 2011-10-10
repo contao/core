facd7545793684ccda69c6438ef059b7f6814a50
Leo Feyer <github@contao.org>
2011-10-10 14:07:59 +0000

    Cleaning up for version 2.10.2

--- system/libraries/Controller.php
+++ system/libraries/Controller.php
@@ -2926,7 +2926,6 @@ abstract class Controller extends System
 
 		// Thanks to Andreas Schempp (see #2475)
 		$arrPages = array_intersect($this->getChildRecords(0, $strTable), $arrPages);
-		$arrPages = array_reverse($arrPages); // see #3423
 		$arrPages = array_values(array_diff($arrPages, $this->getChildRecords($arrPages, $strTable)));
 
 		return $arrPages;
