539fb5ab3bd0a20d5c2d42c50cbb78f0b15cf68c
Dominik Zogg <dominik.zogg@gmail.com>
2011-12-04 11:20:54 +0100

    Merge pull request #3672 from dominikzogg/master

--- system/modules/calendar/ModuleEventlist.php
+++ system/modules/calendar/ModuleEventlist.php
@@ -313,6 +313,8 @@ class ModuleEventlist extends Events
 			$strEvents = "\n" . '<div class="empty">' . $strEmpty . '</div>' . "\n";
 		}
 
+		// See #3672
+		$this->Template->headline = $this->headline;
 		$this->Template->events = $strEvents;
 
 		// Clear the $_GET array (see #2445)
