a211e092f3d638a211ee3af051295a3e9f4b6d1c
Leo Feyer <github@contao.org>
2012-04-21 15:29:47 +0200

    Resize the debug bar if the browser window is resized (see #4172)

--- system/library/Contao/Template.php
+++ system/library/Contao/Template.php
@@ -320,6 +320,9 @@ abstract class Template extends Controller
 							. "Cookie.write('CONTAO_CONSOLE',$('debug').hasClass('closed')?'closed':'');"
 							. "document.body.setStyle('margin-bottom', $('debug').hasClass('closed')?'60px':'320px');"
 						. "});"
+						. "window.addEvent('resize',function() {"
+							. "$$('#debug p','#debug div').setStyle('width',window.getSize().x);"
+						. "});"
 					. "})(document.id);"
 				. '</script>' . "\n\n";
 
