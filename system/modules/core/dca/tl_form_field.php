5ee6629ac7543812caa2fece8c5dc554057b453c
Leo Feyer <github@contao.org>
2012-04-30 14:52:31 +0200

    Show the validation options for hidden fields (see #4126)

--- system/modules/core/dca/tl_form_field.php
+++ system/modules/core/dca/tl_form_field.php
@@ -136,7 +136,7 @@ $GLOBALS['TL_DCA']['tl_form_field'] = array
 		'radio'                       => '{type_legend},type,name,label;{fconfig_legend},mandatory;{options_legend},options;{expert_legend:hide},class;{submit_legend},addSubmit',
 		'checkbox'                    => '{type_legend},type,name,label;{fconfig_legend},mandatory;{options_legend},options;{expert_legend:hide},class;{submit_legend},addSubmit',
 		'upload'                      => '{type_legend},type,name,label;{fconfig_legend},mandatory,extensions,maxlength;{store_legend:hide},storeFile;{expert_legend:hide},class,accesskey,tabindex,fSize;{submit_legend},addSubmit',
-		'hidden'                      => '{type_legend},type,name,value',
+		'hidden'                      => '{type_legend},type,name,value;{fconfig_legend},mandatory,rgxp',
 		'captcha'                     => '{type_legend},type,label;{expert_legend:hide},class,accesskey,tabindex;{submit_legend},addSubmit',
 		'submit'                      => '{type_legend},type,slabel;{image_legend:hide},imageSubmit;{expert_legend:hide},class,accesskey,tabindex'
 	),
