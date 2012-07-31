5050f0fd81acefb9fcbf8f04a709c7bda57b4660
Leo Feyer <github@contao.org>
2012-07-31 16:46:12 +0200

    Updated the mediaelement.js plugin to version 2.9.1

--- system/config/constants.php
+++ system/config/constants.php
@@ -27,7 +27,7 @@ define('HTML5SHIV', '3.6');
 define('JQUERY', '1.7.1');
 define('JQUERY_UI', '1.8.17');
 define('COLORBOX', '1.3.19');
-define('MEDIAELEMENT', '2.8.1');
+define('MEDIAELEMENT', '2.9.1');
 define('COLORPICKER', '1.3');
 define('TABLESORTER', '2.0.5');
 define('MOOTOOLS', '1.4.5');
