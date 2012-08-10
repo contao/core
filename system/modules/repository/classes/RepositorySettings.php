f1437d7c8980df78fba7421795f5b1fd0b116e78
Leo Feyer <github@contao.org>
2012-08-10 16:49:49 +0200

    Bumped the version number so I don't forget it later :)

--- system/modules/repository/classes/RepositorySettings.php
+++ system/modules/repository/classes/RepositorySettings.php
@@ -17,6 +17,7 @@ define('REPOSITORY_SOAPCACHE', true);
 
 // valid core versions in descending order
 define('REPOSITORY_COREVERSIONS',
+		'30000009,30000009;'.	// 3.0.0 stable
 		'30000006,30000006;'.	// 3.0.0 RC1
 		'30000003,30000003;'.	// 3.0.0 beta1
 		'20110059,20110059;'.	// 2.11.5 stable
