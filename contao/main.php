993dd1680d6528d013fdf65abc539f71156eea1e
Leo Feyer <github@contao.org>
2012-08-15 16:36:45 +0200

    Added the missing `ampersand()` to the "latest changes" table (see #4633)

--- contao/main.php
+++ contao/main.php
@@ -196,7 +196,7 @@ class Main extends Backend
 
 			if ($arrRow['editUrl'] != '')
 			{
-				$arrRow['editUrl'] = preg_replace('/&(amp;)?rt=[a-f0-9]+/', '&amp;rt=' . REQUEST_TOKEN, $arrRow['editUrl']);
+				$arrRow['editUrl'] = preg_replace('/&(amp;)?rt=[a-f0-9]+/', '&amp;rt=' . REQUEST_TOKEN, ampersand($arrRow['editUrl']));
 			}
 
 			$arrVersions[] = $arrRow;
