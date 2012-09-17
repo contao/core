0260a1796f08418921a0629abb3f8d1d89aba855
Tristan Lins <tristan.lins@infinitysoft.de>
2012-09-17 07:53:37 +0200

    Replace for-loop to generate columns with str_repeat in findByPks.

--- system/modules/core/library/Contao/Model.php
+++ system/modules/core/library/Contao/Model.php
@@ -421,16 +421,9 @@ abstract class Model extends \System
 			return null;
 		}
 
-		// build columns array
-		$arrColumns = array();
-		for ($i = 0; $i < count($arrPks); $i++)
-		{
-			$arrColumns[] = '?';
-		}
-
 		$arrOptions = array_merge($arrOptions, array
 		(
-			'column' => array(static::$strTable . '.' . static::$strPk . ' IN (' . implode(',', $arrColumns) . ')'),
+			'column' => array(static::$strTable . '.' . static::$strPk . ' IN (' . rtrim(str_repeat('?,', count($arrPks)), ',') . ')'),
 			'value'  => $arrPks,
 			'return' => 'Collection'
 		));
