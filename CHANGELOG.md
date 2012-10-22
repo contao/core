f0c03ff44df41a74f59dd29d7206d8520bbe62fd
Leo Feyer <github@contao.org>
2012-10-22 17:41:33 +0200

    Updated SwiftMailer to version 4.2.1 (see #4935)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ Contao Open Source CMS Changelog
 Version 2.11.7 (2012-XX-XX)
 ---------------------------
 
+### Updated
+Updated SwiftMailer to version 4.2.1 (see #4935).
+
 ### Fixed
 Set the file permissions depending on the server's umask setting (see #4941).
 
--- plugins/swiftmailer/classes/Swift/MemorySpool.php
+++ plugins/swiftmailer/classes/Swift/MemorySpool.php
@@ -15,62 +15,68 @@
  */
 class Swift_MemorySpool implements Swift_Spool
 {
-  protected $messages = array();
+    protected $messages = array();
 
-  /**
-   * Tests if this Transport mechanism has started.
-   * @return boolean
-   */
-  public function isStarted()
-  {
-    return true;
-  }
-
-  /**
-   * Starts this Transport mechanism.
-   */
-  public function start()
-  {
-  }
-
-  /**
-   * Stops this Transport mechanism.
-   */
-  public function stop()
-  {
-  }
-
-  /**
-   * Stores a message in the queue.
-   *
-   * @param Swift_Mime_Message $message The message to store
-   */
-  public function queueMessage(Swift_Mime_Message $message)
-  {
-    $this->messages[] = $message;
-  }
-
-  /**
-   * Sends messages using the given transport instance.
-   *
-   * @param Swift_Transport $transport         A transport instance
-   * @param string[]        &$failedRecipients An array of failures by-reference
-   *
-   * @return int The number of sent emails
-   */
-  public function flushQueue(Swift_Transport $transport, &$failedRecipients = null)
-  {
-    if (!$transport->isStarted())
+    /**
+     * Tests if this Transport mechanism has started.
+     * @return boolean
+     */
+    public function isStarted()
     {
-      $transport->start();
+        return true;
     }
 
-    $count = 0;
-    while ($message = array_pop($this->messages))
+    /**
+     * Starts this Transport mechanism.
+     */
+    public function start()
     {
-      $count += $transport->send($message, $failedRecipients);
     }
 
-    return $count;
-  }
+    /**
+     * Stops this Transport mechanism.
+     */
+    public function stop()
+    {
+    }
+
+    /**
+     * Stores a message in the queue.
+     *
+     * @param Swift_Mime_Message $message The message to store
+     *
+     * @return boolean Whether the operation has succeeded
+     */
+    public function queueMessage(Swift_Mime_Message $message)
+    {
+        $this->messages[] = $message;
+
+        return true;
+    }
+
+    /**
+     * Sends messages using the given transport instance.
+     *
+     * @param Swift_Transport $transport A transport instance
+     * @param string[]        &$failedRecipients An array of failures by-reference
+     *
+     * @return int The number of sent emails
+     */
+    public function flushQueue(Swift_Transport $transport, &$failedRecipients = null)
+    {
+        if (!$this->messages) {
+            return 0;
+        }
+
+        if (!$transport->isStarted()) {
+            $transport->start();
+        }
+
+        $count = 0;
+        while ($message = array_pop($this->messages)) {
+            $count += $transport->send($message, $failedRecipients);
+        }
+
+        return $count;
+    }
 }
