783a7159b7071713bbbe55ba27ad79ea5fe7a44f
Leo Feyer <github@contao.org>
2012-10-29 11:47:32 +0100

    Updated SwiftMailer to version 4.2.2 (see #4966)

--- system/vendor/swiftmailer/classes/Swift/Mailer.php
+++ system/vendor/swiftmailer/classes/Swift/Mailer.php
@@ -12,7 +12,7 @@
  * Swift Mailer class.
  *
  * @package Swift
- * @author Chris Corbyn
+ * @author  Chris Corbyn
  */
 class Swift_Mailer
 {
@@ -32,7 +32,8 @@ class Swift_Mailer
     /**
      * Create a new Mailer instance.
      *
-     * @param  Swift_Transport $transport
+     * @param Swift_Transport $transport
+     *
      * @return Swift_Mailer
      */
     public static function newInstance(Swift_Transport $transport)
@@ -41,10 +42,12 @@ class Swift_Mailer
     }
 
     /**
-     * Create a new class instance of one of the message services
+     * Create a new class instance of one of the message services.
+     *
      * For example 'mimepart' would create a 'message.mimepart' instance
      *
-     * @param  string $service
+     * @param string $service
+     *
      * @return object
      */
     public function createMessage($service = 'message')
@@ -65,8 +68,9 @@ class Swift_Mailer
      * delivery.
      *
      * @param Swift_Mime_Message $message
-     * @param array &$failedRecipients, optional
-     * @return int
+     * @param array              $failedRecipients An array of failures by-reference
+     *
+     * @return integer
      */
     public function send(Swift_Mime_Message $message, &$failedRecipients = null)
     {
@@ -93,7 +97,6 @@ class Swift_Mailer
      * Register a plugin using a known unique key (e.g. myPlugin).
      *
      * @param Swift_Events_EventListener $plugin
-     * @param string                     $key
      */
     public function registerPlugin(Swift_Events_EventListener $plugin)
     {
@@ -102,6 +105,7 @@ class Swift_Mailer
 
     /**
      * The Transport used to send messages.
+     *
      * @return Swift_Transport
      */
     public function getTransport()
