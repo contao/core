ca1793c242a4909db05884676c79bcf579fb2a13
Leo Feyer <github@contao.org>
2012-11-07 17:12:20 +0100

    Added all language source files (to be able to simulate branches on transifex.com)

--- system/modules/repository/languages/ro/tl_settings.php
+++ system/modules/repository/languages/ro/tl_settings.php
@@ -14,12 +14,12 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
+$GLOBALS['TL_LANG']['tl_settings']['repository_wsdl'][0] = 'Adresa arhivei de extensii';
+$GLOBALS['TL_LANG']['tl_settings']['repository_wsdl'][1] = 'Introduceţi URL-ul fişierului WSDL pentru acces la arhiva de extensii';
 $GLOBALS['TL_LANG']['tl_settings']['repository_languages'][0] = 'Limbi preferate';
 $GLOBALS['TL_LANG']['tl_settings']['repository_languages'][1] = 'O listă de coduri ISO de limbi separate prin virgulă, pentru a fi folosită ca preferinţă când o extensie nu este disponibilă în limba aleasă pentru back-end.';
 $GLOBALS['TL_LANG']['tl_settings']['repository_listsize'][0] = 'Extensii pe pagină';
 $GLOBALS['TL_LANG']['tl_settings']['repository_listsize'][1] = 'Numărul de extensii afişate pe pagină.';
 $GLOBALS['TL_LANG']['tl_settings']['repository_unsafe_catalog'][0] = 'Arată extensiile incompatibile';
 $GLOBALS['TL_LANG']['tl_settings']['repository_unsafe_catalog'][1] = 'Arată extensiile incompatibile în catalogul de extensii. Instalarea de extensii incompatibile poate corupe aplicaţia Contao!';
-$GLOBALS['TL_LANG']['tl_settings']['repository_wsdl'][0] = 'Adresa arhivei de extensii';
-$GLOBALS['TL_LANG']['tl_settings']['repository_wsdl'][1] = 'Introduceţi URL-ul fişierului WSDL pentru acces la arhiva de extensii';
 $GLOBALS['TL_LANG']['tl_settings']['repository_legend'] = 'Arhiva de extensii';
