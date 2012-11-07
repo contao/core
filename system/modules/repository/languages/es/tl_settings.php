1b1dcda6cc0c0a5a4542c9ab9aaccf07c94e189e
Leo Feyer <github@contao.org>
2012-11-07 16:46:14 +0100

    Updated all language files

--- system/modules/repository/languages/es/tl_settings.php
+++ system/modules/repository/languages/es/tl_settings.php
@@ -14,12 +14,12 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
+$GLOBALS['TL_LANG']['tl_settings']['repository_wsdl'][0] = 'URL del repositorio de extensiones';
+$GLOBALS['TL_LANG']['tl_settings']['repository_wsdl'][1] = 'Aqui puedes ingresar una URL de algún repositorio de extensiones (archivo WSDL).';
 $GLOBALS['TL_LANG']['tl_settings']['repository_languages'][0] = 'Idiomas de reserva';
 $GLOBALS['TL_LANG']['tl_settings']['repository_languages'][1] = 'Aquí puedes escribir en una lista separada por comas los códigos ISO de los idiomas que se utilizarán si la descripción de la extensión no está disponible en el idioma actual de la sección administrativa.';
 $GLOBALS['TL_LANG']['tl_settings']['repository_listsize'][0] = 'Extensiones por página';
 $GLOBALS['TL_LANG']['tl_settings']['repository_listsize'][1] = 'Aquí puedes definir el número de extensiones mostradas por página.';
 $GLOBALS['TL_LANG']['tl_settings']['repository_unsafe_catalog'][0] = 'Mostrar extensiones no compatibles';
 $GLOBALS['TL_LANG']['tl_settings']['repository_unsafe_catalog'][1] = 'Mostrar extensiones no compatibles en el catálogo de extensiones. Advertencia: instalar extensiones no compatibles puede hacer que su instalación de Contao deje de funcionar.';
-$GLOBALS['TL_LANG']['tl_settings']['repository_wsdl'][0] = 'URL del repositorio de extensiones';
-$GLOBALS['TL_LANG']['tl_settings']['repository_wsdl'][1] = 'Aqui puedes ingresar una URL de algún repositorio de extensiones (archivo WSDL).';
 $GLOBALS['TL_LANG']['tl_settings']['repository_legend'] = 'Repositorio de extensiones';
