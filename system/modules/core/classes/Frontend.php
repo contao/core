24f3cf09676ef2d73eaf3194593a67c635529b5d
Leo Feyer <github@contao.org>
2013-01-15 16:51:43 +0100

    Never pass `null` to `Input::setGet()` in `Frontend::getPageIdFromUrl()` so arguments with empty values are set

--- system/modules/core/classes/Frontend.php
+++ system/modules/core/classes/Frontend.php
@@ -265,7 +265,7 @@ abstract class Frontend extends \Controller
 				return false;
 			}
 
-			\Input::setGet($arrFragments[$i], $arrFragments[$i+1]);
+			\Input::setGet($arrFragments[$i], (string) $arrFragments[$i+1], true);
 		}
 
 		return $arrFragments[0] ?: null;
