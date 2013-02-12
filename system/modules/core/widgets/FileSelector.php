33cf4d55e08f3b10d2137247349f5d11685c6df3
Leo Feyer <github@contao.org>
2013-02-12 15:01:23 +0100

    Correctly search for files as regular user (see #5348)

--- system/modules/core/widgets/FileSelector.php
+++ system/modules/core/widgets/FileSelector.php
@@ -127,7 +127,7 @@ class FileSelector extends \Widget
 
 					while ($objRoot->next())
 					{
-						if (count(array_intersect($this->User->filemounts, $this->Database->getParentRecords($objRoot->id, 'tl_files'))) > 0)
+						if (count(array_intersect($this->User->filemountIds, $this->Database->getParentRecords($objRoot->id, 'tl_files'))) > 0)
 						{
 							$arrRoot[] = $objRoot->id;
 						}
