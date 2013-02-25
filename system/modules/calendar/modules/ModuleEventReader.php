ea483dcd44e582f3bf4c947c9a06a120052750e0
Leo Feyer <github@contao.org>
2013-02-25 20:13:30 +0100

    Remove a duplicate request parameter in the event reader module (see #5382)

--- system/modules/calendar/modules/ModuleEventReader.php
+++ system/modules/calendar/modules/ModuleEventReader.php
@@ -49,7 +49,7 @@ class ModuleEventReader extends \Events
 			$objTemplate->title = $this->headline;
 			$objTemplate->id = $this->id;
 			$objTemplate->link = $this->name;
-			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
 
 			return $objTemplate->parse();
 		}
