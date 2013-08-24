3cbd1a292e4d1e7f468a2c84f57429e3c0f97adc
Tristan Lins <tristan.lins@bit3.de>
2013-08-24 13:43:46 +0200

    Use int code instead of constant E_USER_DEPRECATED.

--- system/functions.php
+++ system/functions.php
@@ -134,7 +134,7 @@ function __error($intType, $strMessage, $strFile, $intLine)
 		E_STRICT            => 'Runtime notice',
 		4096                => 'Recoverable error',
 		8192                => 'Deprecated notice',
-		E_USER_DEPRECATED   => 'Deprecated notice'
+		16384               => 'Deprecated notice'
 	);
 
 	// Ignore functions with an error control operator (@function_name)
