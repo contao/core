7ccbad008cf86fd69de206264a8aa92426edf429
Tristan Lins <tristan.lins@bit3.de>
2013-08-29 16:32:49 +0200

    Add a registry to each database connection.

--- system/modules/core/library/Contao/Database.php
+++ system/modules/core/library/Contao/Database.php
@@ -63,6 +63,12 @@ abstract class Database
 	 */
 	protected $arrCache = array();
 
+	/**
+	 * Model registry
+	 * @var \Model\Registry
+	 */
+	protected $objModelRegistry;
+
 
 	/**
 	 * Establish the database connection
@@ -75,6 +81,7 @@ abstract class Database
 	{
 		$this->arrConfig = $arrConfig;
 		$this->connect();
+		$this->objModelRegistry = new \Model\Registry($this);
 
 		if (!is_resource($this->resConnection) && !is_object($this->resConnection))
 		{
@@ -161,6 +168,17 @@ abstract class Database
 		}
 
 		return static::$arrInstances[$strKey];
+	}
+
+
+	/**
+	 * Return the model registry bound to this connection.
+	 *
+	 * @return \Model\Registry The model registry.
+	 */
+	public function getModelRegistry()
+	{
+		return $this->objModelRegistry;
 	}
 
 
