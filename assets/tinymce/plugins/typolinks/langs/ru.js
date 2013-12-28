d8f1ce6c90150c78c93d51bd5662128371ce4144
Leo Feyer <github@contao.org>
2013-12-28 19:37:27 +0100

    Use an encoded Russian language file in the „typolinks“ plugin (see #6224)

--- assets/tinymce/plugins/typolinks/langs/ru.js
+++ assets/tinymce/plugins/typolinks/langs/ru.js
@@ -1,4 +1,1 @@
-tinyMCE.addI18n('ru.typolinks',{
-link_desc:'Вставить/редактировать ссылку',
-image_desc:'Вставить/редактировать изображение в лайтбоксе'
-});
+tinyMCE.addI18n("ru.typolinks",{link_desc:"\u0412\u0441\u0442\u0430\u0432\u0438\u0442\u044c/\u0438\u0437\u043c\u0435\u043d\u0438\u0442\u044c \u0441\u0441\u044b\u043b\u043a\u0443 Contao",image_desc:"\u0412\u0441\u0442\u0430\u0432\u0438\u0442\u044c/\u0438\u0437\u043c\u0435\u043d\u0438\u0442\u044c \u043c\u0438\u043d\u0438\u0430\u0442\u044e\u0440\u0443 Contao"});
