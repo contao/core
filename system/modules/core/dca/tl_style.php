bedfbc9c28f4957b347aa70aa80529f23233187f
Leo Feyer <github@contao.org>
2014-01-23 13:15:39 +0100

    Restore support for searching the „custom CSS code“ field

--- system/modules/core/dca/tl_style.php
+++ system/modules/core/dca/tl_style.php
@@ -593,6 +593,7 @@ $GLOBALS['TL_DCA']['tl_style'] = array
 		'own' => array
 		(
 			'label'                   => &$GLOBALS['TL_LANG']['tl_style']['own'],
+			'search'                  => true,
 			'inputType'               => 'textarea',
 			'eval'                    => array('preserveTags'=>true, 'style'=>'height:120px'),
 			'sql'                     => "text NULL"
