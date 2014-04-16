c40d7f25a881fca1e4a2b7cafefb521af0aa50e7
Leo Feyer <github@contao.org>
2014-04-16 10:01:29 +0200

    Remove two redundant "TYPOlight" references

--- assets/contao/js/core-uncompressed.js
+++ assets/contao/js/core-uncompressed.js
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2005-2014 Leo Feyer
  *
- * @package Typolinks
+ * @package Core
  * @see     https://contao.org
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
