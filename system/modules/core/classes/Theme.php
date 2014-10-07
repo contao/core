5d0d6ed937c15e3f337541d60fc4795f9a61ded1
Martin Auswöger <martin@auswoeger.com>
2014-10-07 11:07:10 +0200

    Generate theme.xml after export theme hooks

--- system/modules/core/classes/Theme.php
+++ system/modules/core/classes/Theme.php
@@ -717,9 +717,6 @@ class Theme extends \Backend
 		$strTmp = md5(uniqid(mt_rand(), true));
 		$objArchive = new \ZipWriter('system/tmp/'. $strTmp);
 
-		// Add the XML document
-		$objArchive->addString($xml->saveXML(), 'theme.xml');
-
 		// Add the folders
 		$arrFolders = deserialize($objTheme->folders);
 
@@ -747,6 +744,9 @@ class Theme extends \Backend
 				\System::importStatic($callback[0])->$callback[1]($xml, $objArchive, $objTheme->id);
 			}
 		}
+
+		// Add the XML document
+		$objArchive->addString($xml->saveXML(), 'theme.xml');
 
 		// Close the archive
 		$objArchive->close();
