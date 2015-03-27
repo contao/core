39d996c63959e1fa7a3160a0c7a7e0d17459fb08
Leo Feyer <github@contao.org>
2015-03-27 20:53:38 +0100

    Fix the coding style of the Backend class

--- system/modules/core/classes/Backend.php
+++ system/modules/core/classes/Backend.php
@@ -674,7 +674,7 @@ abstract class Backend extends \Controller
 		$arrPages = array();
 
 		// Recursively walk through all subpages
-		while($objPages->next())
+		while ($objPages->next())
 		{
 			// Set domain
 			if ($objPages->type == 'root')
