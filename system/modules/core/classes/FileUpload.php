b56e1aac747289dafa2c39203116d28e6ba58d33
Leo Feyer <github@contao.org>
2015-07-20 10:33:15 +0200

    Remove an unused method argument in the FileUpload class.

--- system/modules/core/classes/FileUpload.php
+++ system/modules/core/classes/FileUpload.php
@@ -176,7 +176,7 @@ class FileUpload extends \Backend
 					if ($this->Files->move_uploaded_file($file['tmp_name'], $strNewFile))
 					{
 						$this->Files->chmod($strNewFile, \Config::get('defaultFileChmod'));
-						$blnResized = $this->resizeUploadedImage($strNewFile, $file);
+						$blnResized = $this->resizeUploadedImage($strNewFile);
 
 						// Notify the user
 						if (!$blnResized)
