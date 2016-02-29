3b099ea9a1b8240e4144e927137b3b6a76de3ef6
Leo Feyer <github@contao.org>
2016-02-29 17:36:28 +0100

    Fix the media element folder name.

--- system/config/constants.php
+++ system/config/constants.php
@@ -33,7 +33,7 @@ define('SWIPE', '2.0');
 define('JQUERY', '1.11.3');
 define('JQUERY_UI', '1.11.4');
 define('COLORBOX', '1.6.1');
-define('MEDIAELEMENT', '2.16.4');
+define('MEDIAELEMENT', '2.19.0.1');
 define('TABLESORTER', '2.0.5');
 define('MOOTOOLS', '1.5.1');
 define('COLORPICKER', '1.4');
