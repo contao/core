6edf92b24f6a2251303b898c5f47f96b9f41c927
David Greminger <david.greminger@1up.io>
2016-03-02 08:54:12 +0100

    Only treat size as an array, if it's not numeric

--- system/modules/core/library/Contao/Controller.php
+++ system/modules/core/library/Contao/Controller.php
@@ -1463,7 +1463,12 @@ abstract class Controller extends \System
 		}
 
 		$imgSize = $objFile->imageSize;
-		$size = deserialize($arrItem['size'], true) + array(0, 0, 'crop');
+		$size = deserialize($arrItem['size']);
+
+		if (!is_numeric($size))
+		{
+			$size = deserialize($arrItem['size'], true) + array(0, 0, 'crop');
+		}
 
 		if ($intMaxWidth === null)
 		{
