068510d1b2ef882a42f44e71e8442462f1d0fc3f
Leo Feyer <github@contao.org>
2016-08-26 14:49:06 +0200

    Fix a wrong class reference.

--- system/modules/core/library/Contao/StringUtil.php
+++ system/modules/core/library/Contao/StringUtil.php
@@ -494,7 +494,7 @@ class StringUtil
 				{
 					if (!array_key_exists($matches[1], $arrData))
 					{
-						System::log(sprintf('Tried to parse unknown simple token "%s".', $matches[1]), __METHOD__, TL_ERROR);
+						\System::log(sprintf('Tried to parse unknown simple token "%s".', $matches[1]), __METHOD__, TL_ERROR);
 
 						return '##' . $matches[1] . '##';
 					}
@@ -518,7 +518,7 @@ class StringUtil
 
 			if (!array_key_exists($strToken, $arrData))
 			{
-				System::log(sprintf('Tried to evaluate unknown simple token "%s".', $strToken), __METHOD__, TL_ERROR);
+				\System::log(sprintf('Tried to evaluate unknown simple token "%s".', $strToken), __METHOD__, TL_ERROR);
 
 				return false;
 			}
