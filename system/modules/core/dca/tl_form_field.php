08189923c2b2841985ab5d4b6fe7e37d5032e67c
Leo Feyer <github@contao.org>
2016-09-05 13:55:08 +0200

    Always convert insert tags to paths in the form field preview (see #8437).

--- system/modules/core/dca/tl_form_field.php
+++ system/modules/core/dca/tl_form_field.php
@@ -602,14 +602,9 @@ class tl_form_field extends Backend
 			return $strType . "\n" . $objWidget->value . "\n</div>\n";
 		}
 
-		if ($objWidget instanceof FormHeadline || $objWidget instanceof FormExplanation)
-		{
-			$strWidget = StringUtil::insertTagToSrc($strWidget);
-		}
-
 		return $strType . '
 <table class="tl_form_field_preview">
-'.$strWidget.'</table>
+'.StringUtil::insertTagToSrc($strWidget).'</table>
 </div>' . "\n";
 	}
 
