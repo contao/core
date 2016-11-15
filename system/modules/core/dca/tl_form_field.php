544a6be7d0f5c07da9674a0cb6c675feab5295f8
Leo Feyer <github@contao.org>
2016-11-15 13:40:07 +0100

    Make tl_content.type and tl_form_field.type 64 characters long (see #8569).

--- system/modules/core/dca/tl_form_field.php
+++ system/modules/core/dca/tl_form_field.php
@@ -167,7 +167,7 @@ $GLOBALS['TL_DCA']['tl_form_field'] = array
 			'options_callback'        => array('tl_form_field', 'getFields'),
 			'eval'                    => array('helpwizard'=>true, 'submitOnChange'=>true),
 			'reference'               => &$GLOBALS['TL_LANG']['FFL'],
-			'sql'                     => "varchar(32) NOT NULL default ''"
+			'sql'                     => "varchar(64) NOT NULL default ''"
 		),
 		'name' => array
 		(
