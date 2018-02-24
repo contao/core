375e3df970512af07645d0e8cb453bca96685bf9
Leo Feyer <github@contao.org>
2018-02-24 21:56:15 +0100

    Fix a phpDoc comment.

--- system/modules/core/dca/tl_member.php
+++ system/modules/core/dca/tl_member.php
@@ -539,7 +539,7 @@ class tl_member extends Backend
 
 
 	/**
-	 * Call the "setNewPassword" callback
+	 * Reset the autologin hash
 	 *
 	 * @param string        $varValue
 	 * @param DataContainer $dc
