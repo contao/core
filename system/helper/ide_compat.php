ee01e08dc0dc9562aca8bb7351f9974d9aee5772
Leo Feyer <github@contao.org>
2018-02-27 21:17:42 +0100

    Update the copyright notices.

--- system/helper/ide_compat.php
+++ system/helper/ide_compat.php
@@ -1,11 +1,11 @@
 <?php
 
-/**
- * Contao Open Source CMS
+/*
+ * This file is part of Contao.
  *
- * Copyright (c) 2005-2016 Leo Feyer
+ * (c) Leo Feyer
  *
- * @license LGPL-3.0+
+ * @license LGPL-3.0-or-later
  */
 
 /**
